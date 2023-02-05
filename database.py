d90471a184b2d258600df949a59eff4e54a917cc
sadewpahasara <sadewpahasar2008@gmail.com>
2023-02-05 18:34:15 +0530

    database again

--- database.py
+++ database.py
@@ -7,16 +7,16 @@ import mysql.connector
 #   database="emobetting_db"
 # )
 
-db = mysql.connector.connect(
-  host="odd-national.cnrmem3900ov.ap-south-1.rds.amazonaws.com",
-  user="admin",
-  password="oddnational0721793599",
-  database="odd_national"
-)
-
 # db = mysql.connector.connect(
-#   host="sql866.main-hosting.eu",
-#   user="u124366181_root",
-#   password="$Theo3474",
-#   database="u124366181_odd_national"
+#   host="odd-national.cnrmem3900ov.ap-south-1.rds.amazonaws.com",
+#   user="admin",
+#   password="oddnational0721793599",
+#   database="odd_national"
 # )
+
+db = mysql.connector.connect(
+  host="sql866.main-hosting.eu",
+  user="u124366181_root",
+  password="$Theo3474",
+  database="u124366181_odd_national"
+)
